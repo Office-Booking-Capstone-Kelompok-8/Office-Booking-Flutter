3c8c8713d4cbfb955c02df4184683c0943bdc840
Fadillah283 <fahmifadillah283@gmail.com>
2022-12-12 19:42:18 +0700

    change api constant

--- lib/utils/constant/api_constant.dart
+++ lib/utils/constant/api_constant.dart
@@ -4,8 +4,8 @@ class Api {
   static const String register = 'auth/register';
   static const String logout = 'auth/logout';
   static const String refreshToken = 'auth/refresh';
-  static const String requestOtp = 'auth/request';
-  static const String verifyOtp = 'auth/verify';
+  static const String requestOtp = 'auth/otp/request/reset-password';
+  static const String verifyOtp = 'auth/otp/verify/reset-password';
   static const String resetPassword = 'auth/reset-password';
   static const String userDetail = 'users';
   static const String changePassword = 'users/change-password';
@@ -14,4 +14,6 @@ class Api {
   static const String buildingFacilities = 'buildings/faclities/category';
   static const String payment = 'payments';
   static const String picture = 'users/picture';
+  static const String requestOtpEmail = 'auth/otp/request/email';
+  static const String verifyOtpEmail = 'auth/otp/verify/email';
 }
